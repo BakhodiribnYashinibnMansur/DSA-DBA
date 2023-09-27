f92d695b263ec5500d4e75400cd60291baf5c9b9
Bakhodir Yashin Mansur <phapp0224mb@gmail.com>
2023-09-27 23:12:49 +0500

    182. Duplicate Emails

---
+++ DBA/LeetCode/182. Duplicate Emails.sql
@@ -0,0 +1,58 @@
+-- 182. Duplicate Emails
+-- Solved
+-- Easy
+-- Topics
+-- Companies
+-- SQL Schema
+-- Pandas Schema
+-- Table: Person
+--
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
+--
+--
+-- Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+--
+-- Return the result table in any order.
+--
+-- The result format is in the following example.
+--
+--
+--
+-- Example 1:
+--
+-- Input:
+-- Person table:
+-- +----+---------+
+-- | id | email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- Output:
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+-- Explanation: a@b.com is repeated two times.
+-- my solution
+SELECT email AS Email FROM Person GROUP BY email HAVING  COUNT(email)>1
+--     Runtime
+-- 763 ms
+-- Beats
+-- 72.23%
+SELECT DISTINCT P1.EMAIL FROM PERSON P1, PERSON P2 WHERE P1.EMAIL = P2.EMAIL AND P1.ID != P2.ID
+--     Runtime
+--     537 ms
+
+SELECT DISTINCT P1.email FROM Person P1 INNER JOIN PERSON P2 ON P1.email = P2.email  WHERE P1.email = P2.email AND P1.id != P2.id
+--    Runtime
+--     843 ms
